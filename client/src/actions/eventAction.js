fa6cf10237581664ff37d8625e8904ca77aa1919
balahari <balahari.balu@gmail.com>
2020-05-03 11:52:08 +0200

    Login partially working

--- client/src/actions/eventAction.js
+++ client/src/actions/eventAction.js
@@ -1,6 +1,9 @@
 import {GET_EVENTS, DELETE_EVENT, ADD_EVENT, EVENTS_LOADING} from './types';
 import axios from 'axios';
 
+import {tokenConfig} from './authActions';
+import {returnErrors} from './errorActions';
+
 export const getEvents = () => dispatch => {
     dispatch(setEventsLoading());
     axios
@@ -9,19 +12,21 @@ export const getEvents = () => dispatch => {
             dispatch({
                 type:GET_EVENTS,
                 payload:res.data
-            })
-            )
+            }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
 };
 
-export const addEvent = item => dispatch => {
+export const addEvent = item => (dispatch,getState) => {
     axios
-        .post('/api/events',item)
+        .post('/api/events',item,tokenConfig(getState))
         .then(res =>
             dispatch({
                 type: ADD_EVENT,
                 payload: res.data
             }))
-
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
 };
 
 export const deleteEvent = id =>{
@@ -29,6 +34,7 @@ export const deleteEvent = id =>{
         type:DELETE_EVENT,
         payload: id
     };
+
 };
 
 
